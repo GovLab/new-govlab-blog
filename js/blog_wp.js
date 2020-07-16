a596fe1552b137e563d8c736b47dc21ec8897479
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2020-07-16 14:26:07 -0400

    blog-landing-draft1

--- js/blog_wp.js
+++ js/blog_wp.js
@@ -21,7 +21,7 @@ main();
 ////////////////////////////////////////////////////////////
 
 
-Vue.use(BootstrapVue);
+// Vue.use(BootstrapVue);
 Vue.use(VueMeta);
 
 
@@ -42,6 +42,7 @@ new Vue({
       },
       totalPages: 100,
       perPage: 1,
+      client_blog:'',
       currentPage: 1,
       postdata: [],
       posts: [],
@@ -66,27 +67,51 @@ new Vue({
 
     this.slug = this.slug[this.slug.length - 1];
 
-    this.fetchHeader();
+    this.client_blog = new DirectusSDK({
+      url:"https://directusdev.thegovlab.com/",
+      project:"thegovlab",
+      storage: window.localStorage
+    });
+
+    this.fetchPosts();
   },
   methods: {
-    fetchHeader() {
-
-      self = this;
-
-      axios(`${self.baseUrl}${self.perPage}`, self.wpFetchHeaders).then(data => {
-        self.totalPages  = data.headers['x-wp-totalpages'];
-        self.fetchPosts();
-      })
-
-},
+//     fetchHeader() {
+//
+//       self = this;
+//
+//       axios(`${self.baseUrl}${self.perPage}`, self.wpFetchHeaders).then(data => {
+//         self.totalPages  = data.headers['x-wp-totalpages'];
+//         self.fetchPosts();
+//       })
+//
+// },
   fetchPosts()
   {
 
+
     self = this;
-    axios.get(`${self.baseUrl}${self.perPage}&page=${self.currentPage}`, self.wpFetchHeaders).then(data => {
-      self.posts = data.data;
-        window.scrollTo(0,0);
-    });
+
+
+
+    self.client_blog.getItems(
+      'blog',
+      {
+        fields: ['*.*']})
+      .then(data => {
+      
+        self.posts = data.data;
+        console.log(self.posts);
+        // Promise.all(data.data.map (function(a,b){ self.list.push(a)}))
+    }).catch(err => {
+    console.log(err);
+  })
+
+    // axios.get(`${self.baseUrl}${self.perPage}&page=${self.currentPage}`, self.wpFetchHeaders).then(data => {
+    //   console.log(data);
+    //   self.posts = data.data;
+    //     window.scrollTo(0,0);
+    // });
   },
     showDesc(eventO) {
       eventO['extended'] = true;
