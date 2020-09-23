28d9c2b60a815b4837c0111cae96142b9f03afb9
_stephanschmidt <stephan-schmidt@users.noreply.github.com>
2020-09-23 15:28:07 -0400

    Update blog_post.js

--- post/js/blog_post.js
+++ post/js/blog_post.js
@@ -39,11 +39,14 @@ new Vue({
         return {
           title: this.meta_title,
           meta: [
-            {content: this.meta_title, property:'og:title'},
-            {content: this.meta_image, property:'og:image'},
-            { content: this.meta_content, property:'og:description'},
-            { content: this.meta_url, property:'og:url'}
-
+            { name:"title", content: this.meta_title, property:'og:title'},
+            { name:"image", content: this.meta_image, property:'og:image'},
+            { name:"description", content: this.meta_content, property:'og:description'},
+            { name:"url", content: this.meta_url, property:'og:url'},
+            { name:"twitter-title", content: this.twitter_title, property:'twitter:title'},
+            { name:"twitter-image", content: this.twitter_image, property:'twitter:image'},
+            { name:"twitter-description", content: this.twitter_desc, property:'twitter:description'},
+            { name:"twitter-site", content: '@thegovlab', property:'twitter:site'}
     ]
     }
   },
