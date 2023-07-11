aa899127e3bba1eeaf5b25bf6f6a9b74327de708
Stephan <schmid91@mail.com>
2023-07-11 14:36:08 -0700

    gtag

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteFonts from 'vite-plugin-fonts'
 import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
@@ -12,21 +11,6 @@ export default defineConfig({
       dirs: 'src/pages',
       extensions: ['vue', 'ts'],
     }),
-    // ViteFonts({
-    //   typekit: {
-    //       /**
-    //        * Typekit project id
-    //        */
-    //       id: 'fnf8omo',
-
-    //       /**
-    //        * enable non-blocking renderer
-    //        *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
-    //        * default: true
-    //        */
-    //       defer: true
-    //     },
-    // })
   ],
   server: {
     host: '0.0.0.0',
@@ -38,12 +22,3 @@ export default defineConfig({
   }
 })
 
-// name: 'blog',
-// title: 'Blog',
-// path: '/blog/:slug?',
-// component: () => import(/* webpackChunkName: "blog" */ './views/Blog.vue'),
-// meta: {
-//     isMain: true,
-//     public: true,  // Allow access to even if not logged in
-//     requireAdmin: false,
-// },
