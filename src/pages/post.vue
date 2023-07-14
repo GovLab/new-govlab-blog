170454dba729dbecef38902e5d7df7a3fcb1500e
Stephan <schmid91@mail.com>
2023-07-14 12:56:30 -0700

    change URL to content.thegovlab.com

--- src/pages/post.vue
+++ src/pages/post.vue
@@ -16,7 +16,7 @@ export default {
 
     data() {
       return {
-        directus: new Directus("https://directus10.thegovlab.com/"),
+        directus: new Directus("https://content.thegovlab.com/"),
         blogslug: this.$route.params.name,
         blogPost:[]
       }
