e2bffff6981e503eecc1fe4fd0d5f58d3a4f8650
Stephan <schmid91@mail.com>
2023-07-21 11:11:58 -0700

    blog redirects from post and archive #2

--- src/pages/post.vue
+++ src/pages/post.vue
@@ -53,7 +53,16 @@ export default {
     {
        this.d9blogpost
       .readByQuery({
-        filter: { slug: { _eq: this.blogslug } },
+        filter: { 
+          _and: [
+            {
+               status: {
+              _eq: "published",
+            },
+            }
+            ],
+          slug: { _eq: this.blogslug } 
+        },
         fields: ["*.*,authors.team_id.*,related_posts.incoming_blog_id.*,related_projects.projects_id.*,related_publications.pub_id.*"],
       })
       .then((bpost) => {
@@ -102,14 +111,6 @@ const count = ref(0)
 
 <template>
 
-  <!-- <h1>{{ blogPost.title }}</h1>
-
-  <div class="card">
-    <div v-html="blogPost.content">
-      </div>
-
-  </div> -->
-
   <div class="topnav">
     <div class="menu-bars">
       <div class="bar-wrap">
@@ -152,58 +153,6 @@ const count = ref(0)
 
   </div>
 <div >
-
-<!-- 
-    <div class="col-wrap">
-      <div class="col-100">
-        <div class="row-wrap center">
-            
-
-          <div class="row-wrap center authors" v-if="blogPost.authors" v-for="member in blogPost.authors">
-            
-            <div v-if="!member.team_id.picture" class="author-thumb"
-              :style="{ backgroundImage: 'url(' + member.team_id.picture_blog2020+ ')' }"></div>
-              <div v-if="member.team_id.picture" class="author-thumb"
-                style="background-image: url('/govlab-logo-wp.png')"></div>
-
-            <a class="author-name" :href="'http://www.thegovlab.org/team.html#' + member.team_id.slug"
-              target="_blank">{{member.team_id.name}}<br><span v-if="member.team_id.title" class="author-title">{{member.team_id.title}}</span></a>
-          </div>
-        </div>
-
-        <div class="row-wrap center authors" v-if="blogPost.authors[0] != null" v-for="member in blogPost.authors">   
-         <span v-if="member.team_id != null"> {{member.team_id}}</span>>
-            <div v-if="member.team_id != null" class="author-thumb"
-              :style="{ backgroundImage: 'url(' + member.team_id.picture.data.thumbnails[3].url+ ')' }"></div>
-                 <div v-if="member.team_id != null" class="author-thumb"
-                style="background-image: url('https://directus.thegovlab.com/uploads/thegovlab/originals/de2f39cc-a1f3-4ff9-b6eb-06b64d18b759.png')"></div> 
-
-            <a v-if="member.team_id != null" class="author-name" :href="'http://www.thegovlab.org/team.html#' + member.team_id.slug"
-              target="_blank">{{member.team_id.name}}<br><span  class="author-title">{{member.team_id.title}}</span></a>
-          </div>
-        </div>
-        <div v-if="blogPost.image"  class="featured-image">
-          <img v-bind:src="blogPost.image">
-        </div>
-        <div v-if="!blogPost.image && blogPost.image_blog2020"  class="featured-image">
-          <img v-bind:src="blogPost.image_blog2020">
-        </div>
-        <div class="blog-text" v-html="blogPost.content">
-        </div>
-        <div class="related-stories column-wrap" v-show="blogPost.related_posts && blogPost.related_posts.length>0" >
-          <p>RELATED STORIES</p>
-          
-          <a class="related-items" v-for="member in blogPost.related_posts"
-            :href="'https://blog.thegovlab.org/post/' + member.incoming_blog_id.slug"
-            target="_blank"><span id="underline" v-html="member.incoming_blog_id.title"></span></a>
-        </div>
-      </div>
-
-
-
-    </div>
-  </div>  -->
-
   <div >
     <div class="row-wrap">
       <div class="col-20" v-if="blogPost.related_publications || blogPost.related_projects">
@@ -254,7 +203,7 @@ const count = ref(0)
         <div v-if="blogPost.image"  class="featured-image">
           <img :src="directus._url+'assets/'+blogPost.image.id">
         </div>
-        <div v-show="!blogPost.image && blogPost.image_blog2020"  class="featured-image">
+        <div v-if="!blogPost.image && blogPost.image_blog2020"  class="featured-image">
           <img :src="blogPost.image_blog2020">
         </div>
       </div>
@@ -276,7 +225,6 @@ const count = ref(0)
             <a href="https://www.thegovlab.org/">Home</a>
             <a href="https://www.thegovlab.org/about.html">About</a>
             <a href="https://www.thegovlab.org/projects.html">Projects</a>
-            <!-- <a href="https://www.thegovlab.org/events.html">Events</a> -->
             <a href="https://www.thegovlab.org/events.html" >Events</a>
             <a href="https://www.thegovlab.org/publications.html">Publications</a>
             <a href="https://www.thegovlab.org/team.html">Team</a>
